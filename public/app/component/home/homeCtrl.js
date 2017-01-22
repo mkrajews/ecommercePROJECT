a4cf49df1222ed1dd09f7058a2b448bf025f237a
mkrajews <mimikrajewski@yahoo.com>
2017-01-21 22:05:39 -0700

    button working to get all products

--- public/app/component/home/homeCtrl.js
+++ public/app/component/home/homeCtrl.js
@@ -3,7 +3,9 @@ angular.module('app').controller('homeCtrl', function($scope, homeSvc) {
   $scope.getAllProducts = function() {
     homeSvc.getAllProducts()
       .then(function(response) {
-          console.log(response);
-      })
+          // console.log(response);
+          $scope.products = response;
+      });
   }
+  // $scope.getAllProducts();
 });
